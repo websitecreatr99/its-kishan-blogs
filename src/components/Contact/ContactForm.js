ab39651da4eaee3b8500f7dcfca2a7e45dad188a
kishan0099-sol <kishan.pahadiya@solytics-partners.com>
2024-08-15 19:28:31 +0530

    add

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -24,7 +24,7 @@ const ContactForm = () => {
     
     try {
       console.log("formData",formData);
-      const response = await fetch('http://localhost:3000/api/contact', {
+      const response = await fetch('https://its-kishan-blogs.vercel.app/api/contact', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
