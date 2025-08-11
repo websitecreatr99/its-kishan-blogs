7967458e696052c537ac07da57200a273c7c305f
Kishan Pahadiya <kishanvir4321@gmail.com>
2025-08-11 15:18:05 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,21 @@
 /** @type {import('next').NextConfig} */
 
-const {withContentlayer} = require("next-contentlayer")
+const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig = {
-    compiler:{
-        removeConsole: true,
-    }
+  compiler: {
+    removeConsole: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store' },
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = withContentlayer({ ...nextConfig });
+module.exports = withContentlayer(nextConfig);
