72d4a3483fc08ce980525b647e57c7621db198f8
Kishan Pahadiya <kishanvir4321@gmail.com>
2026-05-22 19:22:31 +0530

    up

--- next.config.js
+++ next.config.js
@@ -1,31 +1,31 @@
 /** @type {import('next').NextConfig} */
 
 const path = require("path");
+const { withContentlayer } = require("next-contentlayer");
 
 const nextConfig = {
   compiler: {
     removeConsole: true,
   },
 
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       react: path.resolve(__dirname, "node_modules/react"),
       "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
-      // Force next-devtools to a dummy empty module
       "next-devtools": path.resolve(__dirname, "empty-module.js"),
     };
+
     return config;
   },
 
   experimental: {
-    // Disable all devtools / segment explorer
     ppr: false,
-    devTools: false,
-    reactCompiler: false,
-    turbo: { rules: {} },
+    turbo: {
+      rules: {},
+    },
   },
 
   devIndicators: {
@@ -34,4 +34,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer(nextConfig);
