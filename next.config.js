519763a2a5202581c256c6b3bf6159558f2ccb84
Kishan Pahadiya <kishanvir4321@gmail.com>
2026-05-22 16:07:25 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,20 +1,41 @@
 /** @type {import('next').NextConfig} */
-const { withContentlayer } = require("next-contentlayer");
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+
+const path = require("path");
 
 const nextConfig = {
   compiler: {
     removeConsole: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Cache-Control', value: 'no-store, max-age=0' },
-        ],
-      },
-    ];
+  swcMinify: false,
+
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+      // Force next-devtools to a dummy empty module
+      "next-devtools": path.resolve(__dirname, "empty-module.js"),
+    };
+    return config;
+  },
+
+  experimental: {
+    // Disable all devtools / segment explorer
+    ppr: false,
+    devTools: false,
+    reactCompiler: false,
+    turbo: { rules: {} },
+  },
+
+  devIndicators: {
+    buildActivity: false,
+    appIsrStatus: false,
   },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = withMDX(nextConfig);
